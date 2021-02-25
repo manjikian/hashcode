a36920d9cea44b22db9805c6634730b2595a8b9e
nwessman <nwessman@kth.se>
2021-02-26 00:24:25 +0100

    Update algorithm to final submission

--- input_parser.py
+++ input_parser.py
@@ -1,37 +1,4 @@
 import sys
-import math
-
-
-def simulate(steps, cars, streets, bonus):
-    result = 0
-
-    # creat the queue for each street
-    queues = {}
-    for street in streets:
-        queues[street] = []
-
-    # place the cars on each street
-    for car in range(len(cars)):
-        queues[cars[car][0]].append([car, 0])
-
-    # start the simulation.
-    for i in range(steps):
-        for street in queues:
-            pop = 0
-            for car in queues[street]:
-                if car[1] == 0:
-                    pop += 1
-                    # find next street
-                    for j in range(len(cars[car[0]])):
-                        if cars[car[0]][j] == street:
-                            if j + 1 < len(cars[car[0]]):
-                                queues[cars[car[0]][j+1]].append([car[0], streets[cars[car[0]][j+1]][2]])
-                            else:
-                                result += bonus
-                                result += steps - i
-                else:
-                    car[1] -= 1
-    return result
 
 
 def readLines(file):
@@ -60,7 +27,6 @@ def solve(file):
 
     matrix = [[None for _ in range(inters)] for _ in range(inters)] # row major
     streets = {}
-    cars = []
 
     for i in range(streetsCount):
         start, end, name, time = input[i+1].split(" ")
@@ -74,7 +40,7 @@ def solve(file):
 
     for i in range(int(carsCount)):
         pathInput = input[int(streetsCount)+1+i].split(" ")[1:]
-        cars.append(pathInput)
+        
         for p in pathInput[:-1]:
             start, end, time = streets[p]
             matrix[start][end][1] += 1
@@ -92,21 +58,16 @@ def solve(file):
             totalPassedCars += matrix[row][col][1]
 
         if len(colSignal) > 0:
-            signal.append((col, colSignal))    
+            signal.append((colSignal, col))    
 
 
     print(len(signal))       
 
     for sig in signal:
-        print(sig[0]) # Print index of intersection
-        print(len(sig[1]))
-        xam = max([a[1] for a in sig[1]])
-
-        abc = sorted(sig[1], key=lambda student: student[0])
-        for s in abc:
-            print(f"{s[2]} { min(int(seconds), max(1, math.floor( 4 * (s[1] / xam))))   }") # Print name of the street and how many seconds
-
-
+        print(sig[1])
+        print(len(sig[0]))
+        for s in sig[0]:
+            print(f"{s[2]} 1") # Print name of the street and how many seconds
 
 inputs = ["a", "b", "c", "d", "e", "f"]
 
@@ -114,9 +75,5 @@ for i in inputs:
     f = open(i+".out", "w")
     sys.stdout = f
     solve(i+".txt")
-
-
-
     f.close()
 
- 
